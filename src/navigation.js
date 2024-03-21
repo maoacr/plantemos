673e1b629933a9025bda04fa2ea9314ea5759968
Mao <maoacr@gmail.com>
2024-03-21 18:05:57 -0500

    last build

--- src/navigation.js
+++ src/navigation.js
@@ -4,66 +4,66 @@ export const headerData = {
   links: [
     {
       text: 'Proyectos',
-      links: [
-        {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        // {
-        //   text: 'About us',
-        //   href: getPermalink('/about'),
-        // },
-        // {
-        //   text: 'Contact',
-        //   href: getPermalink('/contact'),
-        // },
-        // {
-        //   text: 'Terms',
-        //   href: getPermalink('/terms'),
-        // },
-        // {
-        //   text: 'Privacy policy',
-        //   href: getPermalink('/privacy'),
-        // },
-      ],
+      // links: [
+      //   {
+      //     text: 'Features (Anchor Link)',
+      //     href: getPermalink('/#features'),
+      //   },
+      //   {
+      //     text: 'Services',
+      //     href: getPermalink('/services'),
+      //   },
+      //   {
+      //     text: 'Pricing',
+      //     href: getPermalink('/pricing'),
+      //   },
+      //   // {
+      //   //   text: 'About us',
+      //   //   href: getPermalink('/about'),
+      //   // },
+      //   // {
+      //   //   text: 'Contact',
+      //   //   href: getPermalink('/contact'),
+      //   // },
+      //   // {
+      //   //   text: 'Terms',
+      //   //   href: getPermalink('/terms'),
+      //   // },
+      //   // {
+      //   //   text: 'Privacy policy',
+      //   //   href: getPermalink('/privacy'),
+      //   // },
+      // ],
     },
-    {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
+    // {
+    //   text: 'Landing',
+    //   links: [
+    //     {
+    //       text: 'Lead Generation',
+    //       href: getPermalink('/landing/lead-generation'),
+    //     },
+    //     {
+    //       text: 'Long-form Sales',
+    //       href: getPermalink('/landing/sales'),
+    //     },
+    //     {
+    //       text: 'Click-Through',
+    //       href: getPermalink('/landing/click-through'),
+    //     },
+    //     {
+    //       text: 'Product Details (or Services)',
+    //       href: getPermalink('/landing/product'),
+    //     },
+    //     {
+    //       text: 'Coming Soon or Pre-Launch',
+    //       href: getPermalink('/landing/pre-launch'),
+    //     },
+    //     {
+    //       text: 'Subscription',
+    //       href: getPermalink('/landing/subscription'),
+    //     },
+    //   ],
+    // },
     {
       text: 'Blog',
       href: getPermalink('/blog'),
